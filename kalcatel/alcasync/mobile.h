c6d6e6132dafc3a92fa9bbb1fb14a2b9d99e68fe
Michal Čihař <michal@cihar.com>
2002-02-14 21:57:47 +0000

    added support for text addresses (phone numbers) in pdu decoding

--- kalcatel/alcasync/mobile.h
+++ kalcatel/alcasync/mobile.h
@@ -24,7 +24,5 @@
 #ifndef MOBILE_H
 #define MOBILE_H
 
-#define NUM_NAT 0x81 /* phone number type for non-'+' (national) numbers */
-#define NUM_INT 0x91 /* phone number type for numbers prefixed by '+' (international) */
 
 #endif
